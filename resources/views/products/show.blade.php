dc1c6b9f88dc9a4bc7c8bc5fb52296a717cca2bb
commandermeme <carlobahog1@gmail.com>
2019-11-29 01:42:59 +0800

    Modifying

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -21,10 +21,10 @@
                 </div>
                 <div class="card-body">
                     <div class="row no-gutters">
-                        <div class="col-sm-12">
+                        <div class="col-md-6">
                             <img class="img-fluid" src="/storage/prod_images/{{ $product->prod_image }}" alt="No image found!">
                         </div>
-                        <div class="col-sm-12">
+                        <div class="col-md-6">
                             <p class="">
                                 {!! $product->description !!}
                             </p>
